99c576223f0ddfc14d0a6095e6a83a402b313d3c
ofk <ofkjpn+github@gmail.com>
2018-02-16 01:49:57 +0900

    Add `yarn start` command and example files

---
+++ examples/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './examples/index.js',
+  output: {
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: path.join(__dirname, '..', 'node_modules'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|otf|ttf|woff2?)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
+  devtool: '#inline-source-map',
+  resolve: {
+    extensions: ['.js'],
+  },
+};
